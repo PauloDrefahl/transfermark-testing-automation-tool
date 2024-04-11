df338c018559a9c097e5e16130c7691db6ce3d71
Dylan Garcia <93214534+DylanG05@users.noreply.github.com>
2024-04-11 15:30:22 -0400

    Create liveVideo.java
    live video tab test class - dylan

---
+++ transfermarkProject/src/liveVideo.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.JavascriptExecutor;
+
+
+public class liveVideo {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/dylangarcia/Documents/SeleniumDrivers/Chromedriver/chromedriver");
+
+        // Create a new instance of the ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate to the main page
+        driver.get("https://www.transfermarkt.com/");
+        Thread.sleep(2000);
+
+        // Switch to the iframe with the specified title
+        driver.switchTo().frame(driver.findElement(By.cssSelector("#sp_message_iframe_953358")));
+        Thread.sleep(2000);
+
+        // Find the accept button within the iframe and click it
+        WebElement acceptButton = driver.findElement(By.xpath("//*[@id=\"notice\"]/div[3]/div[1]/div/button"));
+        acceptButton.click();
+
+        // Switch back to the main page
+        driver.switchTo().defaultContent();
+        Thread.sleep(3000);
+
+        WebElement liveBtn = driver.findElement(By.xpath("//*[@id=\"main\"]/header/nav/ul/li[7]/a"));
+        liveBtn.click();
+        Thread.sleep(2000);
+
+        scrollDown(driver, 1000);
+        Thread.sleep(2000);
+
+        driver.quit();
+    }
+
+    public static void scrollDown(WebDriver driver, int pixels) {
+        // Create an instance of JavascriptExecutor
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        // Execute JavaScript code to scroll down the page
+        js.executeScript("window.scrollBy(0, " + pixels + ");");
+    }
+}
